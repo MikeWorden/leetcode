aa46cc5412fe60be2a356e09be0f31bfc29646c4
Mike Worden <michael.worden@icloud.com>
2022-10-28 17:03:16 -0600

    Add Add Two Number solution

---
+++ addtwonumbers.py
@@ -0,0 +1,22 @@
+
+from typing import Optional                                                     
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val 
+        self.next = next
+
+def lst2link(lst):
+    cur = dummy = ListNode(0)
+    for e in lst:
+        cur.next = ListNode(e)
+        cur = cur.next
+    return dummy.next
+
+def link2lst(link):
+    newList = []
+    while link is not None:
+        newList.append(link.val)
+        link = link.next
+    return newList
